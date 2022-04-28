50b5be5d47f0e6f66d1467e3f1576cc0ad3a9cfa
Lera <valerya.sharandina@gmail.com>
2022-04-28 15:31:08 +0300

    Task compleet

---
+++ HomeWork1/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Task2
+*/
+Console.Write("Введите первое число:  ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:  ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число:  ");
+int Number3 = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[2];
+int number = Convert.ToInt32(Console.ReadLine())
+for (int i = 0; i<2; if++)
+{
+    
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая на вход
+принимает два числа и выдаёт, какое число большее, а
+какое меньшее.
+*/
+Console.Write ("Введите первое число:  ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число:  ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (Number1 > Number2)
+{
+    Console.WriteLine(max = (Number1));
+
+}
+if (Number1 == Number2)
+{
+    Console.WriteLine("числа равны");
+}
+if (Number1 < Number2)
+{
+    Console.WriteLine(max = (Number2));
+}
+
+
