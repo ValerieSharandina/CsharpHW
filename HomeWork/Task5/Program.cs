c1c50025b1171e5b2229e3dd195bebe7f16db15e
Lera <valerya.sharandina@gmail.com>
2022-05-17 10:31:02 +0300

    решила задачи 5 и 7

---
+++ HomeWork/Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int randomNumber = new Random().Next(100, 1000); 
+
+int numberOne = randomNumber % 10; 
+int numberTwo = randomNumber % 100; 
+int Result = (numberTwo - numberOne) / 10; 
+
+Console.WriteLine (randomNumber);
+Console.WriteLine ($" Число {randomNumber}  Результат {Result}");
+
