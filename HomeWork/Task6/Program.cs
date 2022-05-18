e18864f0e0882c4cb9a093e39dcbbf704d6d74b9
Lera <valerya.sharandina@gmail.com>
2022-05-18 10:46:04 +0300

    решила вторую задачу

--- HomeWork/Task6/Program.cs
+++ HomeWork/Task6/Program.cs
@@ -6,5 +6,22 @@
 32679 -> 6
 */
 
-int randomNumber = new Random().Next(100, 10000); 
+Console.Write ("Введите число:  ");
+int Number = Convert.ToInt32(Console.ReadLine());
 
+if (Number < 100)
+{
+    Console.WriteLine ($"число {Number} двузначное, третьей цифры нет");
+}
+else if (Number > 99 && Number < 1000)
+{
+  int NumberThree = Number % 10 ; 
+  int Result = NumberThree;
+  Console.WriteLine ($"число {Number} трехзначное, третья цифра  {Result}");
+}
+else if (Number > 999 && Number < 10000)
+{
+   int NumberFour = (Number % 100) / 10; 
+  int Result = NumberFour;
+  Console.WriteLine ($"число {Number} четырехзначное, третья цифра  {Result}");
+}
