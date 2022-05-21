ccda68a9ab5f3c70bc9bf58d28aaa85708036487
Lera <valerya.sharandina@gmail.com>
2022-05-21 11:52:46 +0300

    решена задача 23

---
+++ HomeWork/Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 4, 9.
+5 -> 1, 8, 27, 64, 125
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Введите число:");
+int Number = Convert.ToInt32(Console.ReadLine());
+int result;
+Console.Write($"{Number} -> ");
+for (int i = 1; i <= Number; i++)
+{
+    result = i * i * i;
+    Console.Write (result);
+    if (i != Number)
+    {
+        Console.Write(" , ");
+    }
+}
