a81a0d1ef5240985886e43657497d3d89aefe042
Lera <valerya.sharandina@gmail.com>
2022-05-23 11:45:00 +0300

    решила задачу 19

--- HomeWork/Task19/Program.cs
+++ HomeWork/Task19/Program.cs
@@ -5,4 +5,24 @@
 12821 -> да
 */
 
-Console.WriteLine("Введите пятизначное число ");
+Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int revNum = num;
+int total = 0;
+int digit;
+
+for (num = revNum; num != 0; num = num / 10)
+{
+    digit = num % 10;
+    total = (total * 10) + digit;
+}
+if (total == revNum)
+{
+    Console.WriteLine($"Число {revNum} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {revNum} не является палиндромом");
+}
+Console.ReadLine();
+
