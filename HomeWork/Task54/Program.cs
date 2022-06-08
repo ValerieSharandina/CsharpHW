a5cd916090fb1c9ae1a746e9ce84524ee281ae87
Lera <valerya.sharandina@gmail.com>
2022-06-08 21:27:30 +0300

    решила задачу 54

---
+++ HomeWork/Task54/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+элементы каждой строки двумерного массива.
+*/
+int[,] Array(int[,] array)
+{
+    for(var i = 0; i < array.GetLongLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLongLength(1); j++)
+        {
+            array[i,j] = new Random().Next(0,10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] printArray)
+{
+    for (var i = 0; i < printArray.GetLength(0); i++)
+    {
+        for (var j = 0; j < printArray.GetLength(1); j++)
+        {
+            Console.Write($" {printArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+
+void SelectionSortMax(int[,] arrayToSort) 
+{    
+    for (int i = 0; i < arrayToSort.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayToSort.GetLength(1) - 1; j++)
+        {
+            for (int count = j + 1; count < arrayToSort.GetLength(1); count++)
+            {
+                if (arrayToSort[i, j] < arrayToSort[i, count])
+                {
+                    int temp = arrayToSort[i, j];
+                    arrayToSort[i, j] = arrayToSort[i, count];
+                    arrayToSort[i, count] = temp;
+                }
+            }
+        }
+    }
+}
+
+var array = new int [5,5];
+int[,] result=Array(array);
+PrintArray(result);
+
+SelectionSortMax(array);
+PrintArray(array);
