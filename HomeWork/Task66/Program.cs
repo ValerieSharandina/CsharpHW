82199dc0e1d44454608ddf7f2045051858ba747d
Lera <valerya.sharandina@gmail.com>
2022-06-13 15:21:00 +0300

    решена задача 66

---
+++ HomeWork/Task66/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumBetweenMN(int numberOne, int numberTwo)
+{
+    if (numberOne < numberTwo)
+    {
+        return numberOne + SumBetweenMN(numberOne + 1, numberTwo);
+    }
+    if (numberOne > numberTwo)
+    {
+        return numberTwo + SumBetweenMN(numberOne, numberTwo + 1);
+    }
+    return numberOne;
+}
+
+int numberM = 1;
+int numberN = 15;
+int result = SumBetweenMN(numberM, numberN);
+Console.WriteLine($"{result}");
