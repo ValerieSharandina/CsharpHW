e2804992e9eb333076df3ac523ebdb10dcf6375a
Lera <valerya.sharandina@gmail.com>
2022-06-13 15:14:13 +0300

    решена задача 64

---
+++ HomeWork/Task64/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 5; N = 1. -> "5, 4, 3, 2, 1"
+M = 8; N = 4. -> "8, 7,6, 5, 4"
+*/
+
+string IntegersMN(int numberOne, int numberTwo)
+{
+    if (numberOne > numberTwo)
+    {
+        return IntegersMN(numberOne, numberTwo+1) + ", " + numberTwo;
+    }
+    else if(numberOne < numberTwo)
+    {
+        return IntegersMN(numberOne+1, numberTwo) + ", " + numberOne;
+    }
+    {
+        return Convert.ToString(numberTwo);
+    }
+}
+
+Console.Write("Введите значение M: ");
+int numberOne=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int numberTwo=Convert.ToInt32(Console.ReadLine());
+string result = IntegersMN(numberOne, numberTwo);
+Console.WriteLine($"{result}");
+
